ae6ff35a1e3dba12f24473f88b8406e8a0d871d0
hct <chestnut.hct@bitsun-inc.com>
2022-06-30 17:59:26 +0800

    测试

--- src/com/hct/StackForQueue/Demo1.java
+++ src/com/hct/StackForQueue/Demo1.java
@@ -20,7 +20,7 @@ import java.util.Stack;
  */
 public class Demo1 {
 
-
+    //test sourcetree
     Stack<Integer> stack1 = new Stack<Integer>();
     Stack<Integer> stack2 = new Stack<Integer>();
 
